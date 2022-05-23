18cc273af408025f033cca499205c46492dd3a9e
Eric Ekholm <eric.ekholm@gmail.com>
2022-05-23 06:45:54 -0400

    define meal struct

--- meal.go
+++ meal.go
@@ -1,5 +1,11 @@
 package dh
 
 type Meal struct {
-	//define the components of a meal here
+	Name       string `json:"name" binding:"required"`
+	Time       int    `json:"time"`
+	Notes      string `json:"notes"`
+	Difficulty int    `json:"difficulty" binding:"gte=1,lte=3"`
+	Link       string `json:"link"`    //see if there's a url validator
+	Prep       string `json:"prep"`    //this to represent stovetop, crockpot, oven, etc
+	Protein    string `json:"protein"` //represent the main protein in the meal
 }
